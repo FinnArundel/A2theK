fcb4cdfa071134f7f58708f73e7abc2944f43a56
FinnArundel <68985116+FinnArundel@users.noreply.github.com>
2021-05-31 19:46:28 +1000

    Update sketch.js

--- Samplers/Sampler final/sketch.js
+++ Samplers/Sampler final/sketch.js
@@ -37,6 +37,8 @@ function preload () {
 function setup() {
   createCanvas(windowWidth, windowHeight );
   textFont (aisemic);
+  
+  getAudioContext().suspend();
 
   index = round (random (0, 5)); // selects random colour from set 
   colour = colours [index];
